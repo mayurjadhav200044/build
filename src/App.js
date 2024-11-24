31ac123d9664e08477769798137d494f68150a60
mayurjadhav200044 <mayurjadhav200044@gmail.com>
2024-11-24 10:00:29 +0530

    n

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Mayur
+          Learn React With Mayur Jadhav
         </a>
       </header>
     </div>
